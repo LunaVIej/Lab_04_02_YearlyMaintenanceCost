7ab01c8acd0c9136def3e9c764708aa6d2cd6b87
lunaviej <lunaviej@mail.uc.edu>
2024-09-23 02:53:30 -0400

    Completed Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int springCost = 85;
+        int summerCost = 250;
+        int fallCost = 380;
+        int winterCost = 290;
+        int yearlyCost = 0;
+        yearlyCost = springCost + summerCost + fallCost + winterCost;
+        System.out.println("The seasonal cost is " + summerCost + " " + springCost + " " + fallCost + " " + winterCost);
+        System.out.println("The yearly maintenance cost is " + yearlyCost);
+
     }
 }
